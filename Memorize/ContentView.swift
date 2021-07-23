0a93ac233f29e3da1d5fadc81148861a7a14c1ad
Pedro Henrique Ferreira Figueiredo <pedro.figueiredo@castgroup.com.br>
2021-07-22 21:10:44 -0300

    Criação do projeto

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Pedro Henrique on 22/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Turma!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
